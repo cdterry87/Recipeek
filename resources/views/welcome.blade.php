d3792edcaa8b8a328945b0ccd60ab35f67fae1f7
= <chase.terry87@gmail.com>
2019-04-03 13:05:16 -0500

    A little more visual enhancements for the template.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
             <div id="navbarTop" class="navbar-menu">
                 <div class="navbar-end">
                     <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">Chase Terry</a>
+                        <a class="navbar-link"><i class="fas fa-user-circle"></i>Chase Terry</a>
                         <div class="navbar-dropdown">
                             <a class="navbar-item"><i class="fas fa-cogs"></i> Settings</a>
                             <hr class="navbar-divider">
@@ -55,32 +55,45 @@
         </div>
     </section>
 
-    <div id="main" class="container">
-        <div class="columns">
-            <div class="column is-one-quarter">
-                <div class="box">
-                    <a class="button is-info is-fullwidth">
-                        <span class="icon">
-                            <i class="fas fa-plus"></i>
-                        </span>
-                        <span>Create Category</span>
-                    </a>
+    <section class="section" id="main">
+        <div class="container">
+            <div class="columns is-multiline">
+                <div class="column is-one-quarter">
+                    <div class="box">
+                        <a class="button is-info is-fullwidth">
+                            <span class="icon">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                            <span>Create Category</span>
+                        </a>
+                    </div>
                 </div>
-
-            </div>
-            <div class="column is-one-quarter">
-                <div class="box">
-                    <h2 class="title is-5 has-text-centered">Category</h2>
-                    <a class="button is-danger is-fullwidth">
-                        <span class="icon">
-                            <i class="fas fa-plus"></i>
-                        </span>
-                        <span>Add Recipe</span>
-                    </a>
+                <div class="column is-one-quarter">
+                    <div class="box">
+                        <h2 class="title is-5 has-text-centered">Category</h2>
+                        <a class="button is-danger is-fullwidth">
+                            <span class="icon">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                            <span>Add Recipe</span>
+                        </a>
+                        <hr>
+                        <div class="buttons">
+                            <button class="button is-fullwidth">
+                                My favorite recipe
+                            </button>
+                            <button class="button is-fullwidth">
+                                A really good recipe
+                            </button>
+                            <button class="button is-fullwidth">
+                                Just another recipe
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <script src="{{ mix('js/bootstrap.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
