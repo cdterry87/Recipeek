1821d37718b939b2dcb5acf3c8117a4738b7738c
= <chase.terry87@gmail.com>
2019-04-06 16:28:44 -0500

    Created the register page.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,73 +2,43 @@
 
 @section('content')
 <div class="container">
-    <div class="columns has-text-centered">
-        <div class="column is-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+    <section class="hero">
+        <div class="hero-body">
+            <div class="columns">
+                <div class="column is-8 is-offset-2">
+                    <h1 class="title is-3 has-text-centered">Recipeek</h1>
+                    <h2 class="subtitle is-5 has-text-centered">Fill out the form below to create an account.</h2>
+                    <div class="box">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="field">
+                                <div class="control">
+                                    <input type="name" class="input is-large" name="name" placeholder="Your name">
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="field">
+                                <div class="control">
+                                    <input type="email" class="input is-large" name="email" placeholder="Your email">
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="field">
+                                <div class="control">
+                                    <input type="password" class="input is-large" name="password" placeholder="Your password">
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            <div class="field">
+                                <div class="control">
+                                    <input type="password" class="input is-large" name="password_confirmation" placeholder="Confirm password">
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            <div class="field is-grouped is-grouped-centered">
+                                <div class="control">
+                                    <button type="submit" class="button is-danger is-medium" name="register">Register</button>
+                                    <a href="{{ route('login') }}" class="button is-link is-medium">Click to Login</a>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
