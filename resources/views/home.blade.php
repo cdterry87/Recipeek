a74e86758e23a988b52f4f884b8bf502879c9673
= <chase.terry87@gmail.com>
2019-04-09 22:25:35 -0500

    Swapped the home page route with the application part of the app and adjusted the controllers to redirect properly depending on if the user is authenticated.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,53 @@
 @extends('layouts.template')
 
 @section('content')
-<section class="hero is-danger">
+<section class="hero is-danger is-medium">
     <div class="hero-body">
         <div class="container">
-            <h1 class="title">Hero title</h1>
-            <h2 class="subtitle">Hero subtitle</h2>
+            <h1 class="title has-text-centered">Get Cookin' with Recipeek!</h1>
         </div>
     </div>
 </section>
+
+<section class="hero">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <h1 class="title is-4">Keep track of your recipes!</h1>
+                    <h2 class="subtitle is-5">
+                        Recipeek helps you keep track of your recipes by giving you a simple way to
+                        categorize and list all of your family's favorites!  Say goodbye to pen and paper,
+                        and put away your old cookbooks.  Organizing your recipes has never been simpler!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="hero">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+
+                </div>
+                <div class="column is-6">
+                    <h1 class="title is-4">Take the guess work out of what's for dinner!</h1>
+                    <h2 class="subtitle is-5">
+                        Not sure what to make for dinner?  We've got you covered!
+                        Recipeek can even suggest new recipes based on your existing recipes.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>This is my footer</p>
+    </div>
+</footer>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,12 @@
 |
 */
 
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/app', function () {
+        return view('app');
+    });
+});
