5e68b9551298be7f4c6c6ce3eebaae1753138d7c
= <chase.terry87@gmail.com>
2019-09-18 12:58:57 -0500

    Added the RecipeController logic and the methods to add a recipe.

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Recipe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RecipeController extends Controller
 {
@@ -13,17 +15,7 @@ class RecipeController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json(Auth::user()->recipes()->get());
     }
 
     /**
@@ -34,51 +26,69 @@ class RecipeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $recipe = Recipe::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'prep_hours' => $request->prep_hours,
+            'prep_minutes' => $request->prep_minutes,
+            'total_hours' => $request->total_hours,
+            'total_minutes' => $request->total_minutes,
+            'servings' => $request->servings,
+            'calories' => $request->calories,
+            'private' => $request->private_recipe,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return response()->json([
+            'status' => (bool) $recipe,
+            'data' => $recipe,
+            'message' => $recipe ? 'Recipe created successfully!' : 'Error adding recipe!'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Recipe  $recipe
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Recipe $recipe)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return response()->json($recipe->where('id', $recipe->id)->first());
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Recipe  $recipe
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Recipe $recipe)
     {
-        //
+        $status = $recipe->update(
+            $request->only(['title', 'description', 'prep_hours', 'prep_minutes', 'total_hours', 'total_minutes', 'servings', 'calories', 'private'])
+        );
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Recipe updated successfully!' : 'Error updating recipe!'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Recipe  $recipe
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Recipe $recipe)
     {
-        //
+        $status = $recipe->delete();
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Recipe deleted successfully!' : 'Error deleting recipe!'
+        ]);
     }
 }
--- resources/js/views/Add.vue
+++ resources/js/views/Add.vue
@@ -10,52 +10,54 @@
                     <div class="mt-3 mb-5" v-if="errors.length > 0">
                         <Errors :errors="errors" />
                     </div>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-text-field hide-details color="white" label="Recipe Name" :rules="[v => !!v || 'Recipe Name is required']" filled prepend-inner-icon="mdi-silverware-fork-knife" id="title" name="title" v-model="title" type="text" required></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-textarea hide-details filled color="white" name="description" prepend-inner-icon="mdi-format-align-justify" id="description" label="Description" v-model="description"></v-textarea>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs6 md3>
-                            <v-text-field hide-details color="white" label="Prep Hours" filled prepend-inner-icon="mdi-alarm" id="prep_hours" name="prep_hours" v-model="prep_hours" type="text" maxlength="2"></v-text-field>
-                        </v-flex>
-                        <v-flex xs6 md3>
-                            <v-text-field hide-details color="white" label="Prep Minutes" filled prepend-inner-icon="mdi-alarm" id="prep_minutes" name="prep_minutes" v-model="prep_minutes" type="text" maxlength="2"></v-text-field>
-                        </v-flex>
-                        <v-flex xs6 md3>
-                            <v-text-field hide-details color="white" label="Total Hours" filled prepend-inner-icon="mdi-alarm" id="total_hours" name="total_hours" v-model="total_hours" type="text" maxlength="2"></v-text-field>
-                        </v-flex>
-                        <v-flex xs6 md3>
-                            <v-text-field hide-details color="white" label="Total Minutes" filled prepend-inner-icon="mdi-alarm" id="total_minutes" name="total_minutes" v-model="total_minutes" type="text" maxlength="2"></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs6 md4>
-                            <v-text-field hide-details color="white" label="Servings" filled prepend-inner-icon="mdi-numeric" id="servings" name="servings" v-model="servings" type="text" maxlength="2"></v-text-field>
-                        </v-flex>
-                        <v-flex xs6 md4>
-                            <v-text-field hide-details color="white" label="Calories" filled prepend-inner-icon="mdi-nutrition" id="calories" name="calories" v-model="calories" type="text" maxlength="6"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 md4>
-                            <v-checkbox hide-details color="white" id="private" name="private" label="Private"></v-checkbox>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout>
-                        <v-flex xs12 class="text-center mt-5">
-                            <v-icon class="mr-2">mdi-information</v-icon>
-                            In the next step, you will add your ingredients!
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row class="text-center mt-5">
-                        <v-flex xs12>
-                            <v-btn outlined type="submit">Add Recipe</v-btn>
-                        </v-flex>
-                    </v-layout>
+                    <v-form method="POST" id="recipeForm" @submit.prevent="addRecipe" ref="form" lazy-validation>
+                        <v-layout row>
+                            <v-flex xs12>
+                                <v-text-field hide-details color="white" label="Recipe Name" :rules="[v => !!v || 'Recipe Name is required']" filled prepend-inner-icon="mdi-silverware-fork-knife" id="title" name="title" v-model="title" type="text" required></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs12>
+                                <v-textarea hide-details filled color="white" name="description" prepend-inner-icon="mdi-format-align-justify" id="description" label="Description" v-model="description"></v-textarea>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs6 md3>
+                                <v-text-field hide-details color="white" label="Prep Hours" filled prepend-inner-icon="mdi-alarm" id="prep_hours" name="prep_hours" v-model="prep_hours" type="text" maxlength="2"></v-text-field>
+                            </v-flex>
+                            <v-flex xs6 md3>
+                                <v-text-field hide-details color="white" label="Prep Minutes" filled prepend-inner-icon="mdi-alarm" id="prep_minutes" name="prep_minutes" v-model="prep_minutes" type="text" maxlength="2"></v-text-field>
+                            </v-flex>
+                            <v-flex xs6 md3>
+                                <v-text-field hide-details color="white" label="Total Hours" filled prepend-inner-icon="mdi-alarm" id="total_hours" name="total_hours" v-model="total_hours" type="text" maxlength="2"></v-text-field>
+                            </v-flex>
+                            <v-flex xs6 md3>
+                                <v-text-field hide-details color="white" label="Total Minutes" filled prepend-inner-icon="mdi-alarm" id="total_minutes" name="total_minutes" v-model="total_minutes" type="text" maxlength="2"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs6 md4>
+                                <v-text-field hide-details color="white" label="Servings" filled prepend-inner-icon="mdi-numeric" id="servings" name="servings" v-model="servings" type="text" maxlength="2"></v-text-field>
+                            </v-flex>
+                            <v-flex xs6 md4>
+                                <v-text-field hide-details color="white" label="Calories" filled prepend-inner-icon="mdi-nutrition" id="calories" name="calories" v-model="calories" type="text" maxlength="6"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-checkbox hide-details color="white" id="private" name="private" label="Private"></v-checkbox>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout>
+                            <v-flex xs12 class="text-center mt-5">
+                                <v-icon class="mr-2">mdi-information</v-icon>
+                                In the next step, you will add your ingredients!
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row class="text-center mt-5">
+                            <v-flex xs12>
+                                <v-btn outlined type="submit">Add Recipe</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-form>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -63,6 +65,7 @@
 </template>
 
 <script>
+    import Event from './../events'
     import Errors from './../components/Errors'
 
     export default {
@@ -85,7 +88,45 @@
             }
         },
         methods: {
+            addRecipe() {
+                if (this.$refs.form.validate()) {
+                    let title = this.title
+                    let description = this.description
+                    let prep_hours = this.prep_hours
+                    let prep_minutes = this.prep_minutes
+                    let total_hours = this.total_hours
+                    let total_minutes = this.total_minutes
+                    let servings = this.servings
+                    let calories = this.calories
+                    let image = this.image
+                    let video = this.video
+                    let private_recipe = this.private
 
+                    axios.post('/api/recipes', { title, description, prep_hours, prep_minutes, total_hours, total_minutes, servings, calories, image, video, private_recipe })
+                    .then(response => {
+                        let recipe_id = response.data.data.id
+
+                        this.$router.push('/ingredients/' + recipe_id)
+
+                        Event.$emit('success', response.data.message)
+
+                        this.reset()
+                    })
+                }
+            },
+            reset() {
+                this.title = ''
+                this.description = ''
+                this.prep_hours = ''
+                this.prep_minutes = ''
+                this.total_hours = ''
+                this.total_minutes = ''
+                this.servings = ''
+                this.calories = ''
+                this.image = ''
+                this.video = ''
+                this.private = ''
+            }
         }
     }
 </script>
