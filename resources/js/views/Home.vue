b753c4443cbcf5b94d6464dce86f5dd71c41a233
= <chase.terry87@gmail.com>
2019-09-23 20:06:01 -0500

    Added an edit mode and made home page dynamic.

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -2,10 +2,10 @@
     <div id="home">
         <v-container fluid grid-list-lg>
             <v-layout row wrap>
-                <v-flex xs12 md4>
+                <v-flex xs12 md4 v-for="(recipe, index) in recipes" :key="index">
                     <v-card light class="mx-auto recipe-card">
                         <v-card-actions>
-                            <v-btn outlined small dark color="red">Start Cooking!</v-btn>
+                            <v-btn outlined small dark color="red" :to="'/recipe/' + recipe.id">Start Cooking!</v-btn>
                             <v-spacer></v-spacer>
                             <v-btn small icon text><v-icon>mdi-share</v-icon></v-btn>
                             <v-btn small icon text><v-icon>mdi-heart</v-icon></v-btn>
@@ -13,8 +13,8 @@
                         <v-card-text>
                             <v-list-item three-line>
                                 <v-list-item-content>
-                                    <v-list-item-title class="headline">My Favorite Recipe</v-list-item-title>
-                                    <v-list-item-subtitle>Some lorem ipsum text lorem ipsum text lorem ipsum text lorem ipsum text lorem ipsum text lorem ipsum text.  Some lorem ipsum text some lorem ipsum text lorem ipsum text.</v-list-item-subtitle>
+                                    <v-list-item-title class="headline" v-html="recipe.title"></v-list-item-title>
+                                    <v-list-item-subtitle v-html="recipe.description"></v-list-item-subtitle>
                                 </v-list-item-content>
                                 <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
                             </v-list-item>
@@ -32,11 +32,28 @@
 </template>
 
 <script>
+    import Loading from './../components/Loading'
+    import Events from './../events'
+
     export default {
         name: 'Home',
+        components: {
+            Loading
+        },
+        data() {
+            return {
+                loading: true,
+                recipes: []
+            }
+        },
         methods: {
             getRecipes() {
+                axios.get('/api/recipes')
+                .then(response => {
+                    this.recipes = response.data
 
+                    this.loading = false
+                })
             }
         },
         created() {
--- resources/js/views/Recipe.vue
+++ resources/js/views/Recipe.vue
@@ -5,6 +5,10 @@
                 <v-flex xs12 md10 offset-md1>
                     <Loading v-if="loading" />
                     <v-card light v-else>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="red" outlined @click="editMode = !editMode"><v-icon>mdi-pencil</v-icon> Edit</v-btn>
+                        </v-card-actions>
                         <v-card-text>
                             <div class="text-center mb-5">
                                 <h1 class="headline">{{ recipe.title }}</h1>
@@ -28,10 +32,10 @@
                             <v-layout row class="mt-3">
                                 <v-flex xs12 md5>
                                     <h2 class="title"><v-icon>mdi-apple</v-icon> Ingredients</h2>
-                                    <div>
+                                    <div v-if="editMode">
                                         <div v-if="showAddIngredient">
                                             <v-form method="POST" id="ingredientForm" class="fadein" @submit.prevent="addIngredient" ref="ingredientForm" lazy-validation>
-                                                <v-text-field hide-details color="grey darken-2" append-icon="mdi-close" @click:append="showAddIngredient = false" label="Ingredient" :rules="[v => !!v || 'Ingredient is required']" id="ingredient" name="ingredient" v-model="ingredient" type="text" required>
+                                                <v-textarea hide-details color="grey darken-2" no-resize append-icon="mdi-close" @click:append="showAddIngredient = false" label="Ingredient" :rules="[v => !!v || 'Ingredient is required']" id="ingredient" name="ingredient" v-model="ingredient" type="text" maxlength="250" rows="3" required>
                                                     <template v-slot:append-outer>
                                                         <v-menu>
                                                             <template v-slot:activator="{ on }">
@@ -41,7 +45,7 @@
                                                             </template>
                                                         </v-menu>
                                                     </template>
-                                                </v-text-field>
+                                                </v-textarea>
                                             </v-form>
                                         </div>
                                         <div v-else>
@@ -50,47 +54,50 @@
                                                 Add Ingredient
                                             </v-btn>
                                         </div>
-                                        <v-list>
-                                            <v-list-item-group multiple>
-                                                <template v-for="(ingredient, index) in recipe.ingredients">
-                                                    <v-list-item :key="ingredient.ingredient">
-                                                        <template v-slot:default="{ active, toggle }">
-                                                            <v-list-item-action>
-                                                                <v-checkbox v-model="active" @click="toggle" class="mx-2" color="red"></v-checkbox>
-                                                            </v-list-item-action>
-                                                            <v-list-item-content>
-                                                                <v-list-item-title>{{ ingredient.ingredient }}</v-list-item-title>
-                                                            </v-list-item-content>
-                                                        </template>
-                                                    </v-list-item>
-                                                    <v-divider v-if="index + 1 < recipe.ingredients.length" :key="index"></v-divider>
-                                                </template>
-                                            </v-list-item-group>
-                                        </v-list>
                                     </div>
+                                    <v-list>
+                                        <v-list-item-group multiple>
+                                            <template v-for="(ingredient, index) in recipe.ingredients">
+                                                <v-list-item :key="ingredient.ingredient">
+                                                    <template v-slot:default="{ active, toggle }">
+                                                        <v-list-item-action>
+                                                            <v-icon v-if="editMode" @click="deleteIngredient(ingredient.id)">mdi-close</v-icon>
+                                                            <v-checkbox v-else v-model="active" @click="toggle" class="mx-2" color="red"></v-checkbox>
+                                                        </v-list-item-action>
+                                                        <v-list-item-content>
+                                                            <v-list-item-title>{{ ingredient.ingredient }}</v-list-item-title>
+                                                        </v-list-item-content>
+                                                    </template>
+                                                </v-list-item>
+                                                <v-divider v-if="index + 1 < recipe.ingredients.length" :key="index"></v-divider>
+                                            </template>
+                                        </v-list-item-group>
+                                    </v-list>
                                 </v-flex>
                                 <v-flex xs12 md7>
                                     <h2 class="title"><v-icon>mdi-format-list-numbered</v-icon> Instructions</h2>
-                                    <div v-if="showAddInstruction">
-                                        <v-form method="POST" id="instructionForm" class="fadein" @submit.prevent="addInstruction" ref="instructionForm" lazy-validation>
-                                            <v-textarea hide-details color="grey darken-2" no-resize append-icon="mdi-close" @click:append="showAddInstruction = false" label="Instruction" :rules="[v => !!v || 'Instruction is required']" id="instruction" name="instruction" v-model="instruction" type="text" required>
-                                                <template v-slot:append-outer>
-                                                    <v-menu>
-                                                        <template v-slot:activator="{ on }">
-                                                            <v-btn type="submit" v-on="on" text color="green">
-                                                                <v-icon>mdi-check-bold</v-icon>
-                                                            </v-btn>
-                                                        </template>
-                                                    </v-menu>
-                                                </template>
-                                            </v-textarea>
-                                        </v-form>
-                                    </div>
-                                    <div v-else>
-                                        <v-btn text small color="red" class="mt-3" @click="showAddInstruction = true">
-                                            <v-icon class="mr-2">mdi-plus-circle</v-icon>
-                                            Add Instruction
-                                        </v-btn>
+                                    <div v-if="editMode">
+                                        <div v-if="showAddInstruction">
+                                            <v-form method="POST" id="instructionForm" class="fadein" @submit.prevent="addInstruction" ref="instructionForm" lazy-validation>
+                                                <v-textarea hide-details color="grey darken-2" no-resize append-icon="mdi-close" @click:append="showAddInstruction = false" label="Instruction" :rules="[v => !!v || 'Instruction is required']" id="instruction" name="instruction" v-model="instruction" rows="3" type="text" required>
+                                                    <template v-slot:append-outer>
+                                                        <v-menu>
+                                                            <template v-slot:activator="{ on }">
+                                                                <v-btn type="submit" v-on="on" text color="green">
+                                                                    <v-icon>mdi-check-bold</v-icon>
+                                                                </v-btn>
+                                                            </template>
+                                                        </v-menu>
+                                                    </template>
+                                                </v-textarea>
+                                            </v-form>
+                                        </div>
+                                        <div v-else>
+                                            <v-btn text small color="red" class="mt-3" @click="showAddInstruction = true">
+                                                <v-icon class="mr-2">mdi-plus-circle</v-icon>
+                                                Add Instruction
+                                            </v-btn>
+                                        </div>
                                     </div>
                                     <v-list>
                                         <v-list-item-group multiple>
@@ -98,7 +105,8 @@
                                                 <v-list-item :key="instruction.ininstructionstruction">
                                                     <template v-slot:default="{ active, toggle }">
                                                         <v-list-item-action>
-                                                            <v-checkbox v-model="active" @click="toggle" class="mx-2" color="red"></v-checkbox>
+                                                            <v-icon v-if="editMode" @click="deleteInstruction(instruction.id)">mdi-close</v-icon>
+                                                            <v-checkbox v-else v-model="active" @click="toggle" class="mx-2" color="red"></v-checkbox>
                                                         </v-list-item-action>
                                                         <v-list-item-content>
                                                             <v-list-item-title>{{ instruction.instruction }}</v-list-item-title>
@@ -131,6 +139,7 @@
         },
         data() {
             return {
+                editMode: false,
                 recipe: '',
                 loading: true,
                 showAddIngredient: false,
@@ -161,6 +170,7 @@
                         Event.$emit('success', response.data.message)
 
                         this.ingredient = ''
+                        this.$refs.ingredient.focus();
                     })
                 }
             },
@@ -177,9 +187,26 @@
                         Event.$emit('success', response.data.message)
 
                         this.instruction = ''
+                        this.$refs.instruction.focus();
                     })
                 }
             },
+            deleteIngredient(id) {
+                axios.delete('/api/ingredients/' + id)
+                .then(response => {
+                    this.getRecipe()
+
+                    Event.$emit('success', response.data.message)
+                })
+            },
+            deleteInstruction(id) {
+                axios.delete('/api/instructions/' + id)
+                .then(response => {
+                    this.getRecipe()
+
+                    Event.$emit('success', response.data.message)
+                })
+            }
         },
         created() {
             this.getRecipe()
