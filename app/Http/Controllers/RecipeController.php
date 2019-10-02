790ede5b8da1c7cc3b1d23ff1502cfa078a20bc1
Harden Wallace <hardenw@adsisoftware.com>
2019-10-01 21:11:12 -0500

    Added functionality to store to allow for a file to be uploaded and the path after storage to be placed onto the recipe database entry

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -26,6 +26,15 @@ class RecipeController extends Controller
      */
     public function store(Request $request)
     {
+        $imageFile = null;
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $imageFile = '/storage/recipe_images/' . $filename;
+            $file->move(storage_path('app/public/recipe_images'), $filename);
+        }
+
         $recipe = Recipe::create([
             'title' => $request->title,
             'description' => $request->description,
@@ -35,6 +44,7 @@ class RecipeController extends Controller
             'cook_minutes' => $request->cook_minutes,
             'servings' => $request->servings,
             'calories' => $request->calories,
+            'image' => $imageFile,
             'private' => $request->private_recipe,
             'user_id' => Auth::user()->id
         ]);
@@ -66,8 +76,25 @@ class RecipeController extends Controller
      */
     public function update(Request $request, Recipe $recipe)
     {
+        $updates = ['title', 'description', 'prep_hours', 'prep_minutes', 'cook_hours', 'cook_minutes', 'servings', 'calories', 'private'];
+
+        if ($request->hasFile('newImage')) {
+            $file = $request->file('newImage');
+
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $imageFile = '/storage/recipe_images/' . $filename;
+            $file->move(storage_path('app/public/recipe_images'), $filename);
+
+            $request->request->add(['image' => $imageFile]);
+            $updates[] = 'image';
+
+            $oldImageParts = explode('/', $recipe->image);
+            $fileToDelete = storage_path('app/public/recipe_images/' . end($oldImageParts));
+            unlink($fileToDelete);
+        }
+
         $status = $recipe->update(
-            $request->only(['title', 'description', 'prep_hours', 'prep_minutes', 'cook_hours', 'cook_minutes', 'servings', 'calories', 'private'])
+            $request->only($updates)
         );
 
         return response()->json([
