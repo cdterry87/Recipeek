a8dddf639c9ee8a3e3fb858510679653c8acac43
= <chase.terry87@gmail.com>
2019-10-06 19:01:32 -0500

    Updated the no results message on the discover page.

--- resources/js/views/Discover.vue
+++ resources/js/views/Discover.vue
@@ -3,12 +3,14 @@
         <Loading v-if="loading" />
         <div v-else>
             <v-container fluid grid-list-lg>
-                <v-layout row wrap>
-                    <div v-if="recipes.length == 0">
-                        There are no recipes to look at right now.  Please check back later.
+                <v-row align="center" justify="center">
+                    <div v-if="recipes.length == 0" class="text-center">
+                        <div class="mt-5 display-1">Sorry :(</div>
+                        <div class="mt-5 title">No recipes are available at this time.</div>
+                        <div class="mt-2 title">Please check back later.</div>
                     </div>
                     <RecipeCard v-else v-for="(recipe, index) in recipes" :key="index" :recipe="recipe" />
-                </v-layout>
+                </v-row>
             </v-container>
         </div>
     </div>
